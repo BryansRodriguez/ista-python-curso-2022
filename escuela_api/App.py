b2ce294ca0137815caf3b4e41205768762459f74
Bryans.rodriguez.18 <bryanpollin367@gmail.com>
2022-07-09 21:21:50 -0500

    flask

--- escuela_api/App.py
+++ escuela_api/App.py
@@ -3,8 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def Index():
+def Inicio():
     return 'Hello word'
 
-if __name__ == '__main__':
-    app.run(port = 5000, debug = True)
