525d01079a87f6ff21459986a14e23161c10c22a
BryansRodriguez <bryanpollin367@gmail.com>
2022-07-12 17:54:31 -0500

    flask personas

--- App.py
+++ App.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+ # Mysql Connection
 app.config['MYSQL_HOST']= 'localhost'
 app.config['MYSQL_USER']= 'root'
 app.config['MYSQL_PASSWORD']= ''
 app.config['MYSQL_DB']= 'apiflask'
 mysql = MySQL(app)
 
+#settings
+app.secret_key='mysecretkey'
+
 @app.route('/')
 def Inicio():
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM personas')
+    data = cur.fetchall()
+    return render_template('index.html', contacts = data)
 
 @app.route('/add_contact', methods=['POST'])
 def add_contact():
@@ -21,17 +29,44 @@ def add_contact():
         cur = mysql.connection.cursor()
         cur.execute('INSERT INTO personas (fullname, phone, email) VALUES (%s, %s, %s)', (fullname, phone, email))
         mysql.connection.commit()
+        flash('Contact Added successfully')
 
-        return 'received'
+        return redirect(url_for('Inicio'))
 
 
-@app.route('/edit')
-def edit_contact():
-    return 'editar caontacto'
+@app.route('/edit/<string:id>')
+def get_contact(id):
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM personas WHERE id = {0}'.format(id))
+    data =cur.fetchall()
+    print(data[0])
+    return render_template('edit_personas.html' , contact = data[0])
 
-@app.route('/delete')
-def delete_contact():
-    return 'eliminar caontacto'
+@app.route('/update/<id>', methods = ['POST'])
+def update_contact(id):
+    if request.method == 'POST':
+        fullname = request.form['fullname']
+        phone = request.form['phone']
+        email = request.form['email']
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            UPDATE personas
+            SET fullname= %s,
+                email = %s,
+                phone= %s
+            WHERE id = %s
+        """, (fullname,email,phone,id))
+    mysql.connection.commit()
+    flash('Contact Updated Successfully')
+    return redirect(url_for('Inicio'))
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur =mysql.connection.cursor()
+    cur.execute('DELETE FROM personas WHERE id = {0}'.format(id))
+    mysql.connection.commit()
+    flash('Contact Removed Successfully')
+    return redirect(url_for('Inicio'))
 
 if __name__ == '__main__':
  app.run(port = 3000, debug = True)
